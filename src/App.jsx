6b7b44dab6ad26f03113d7385b0a42b693eba6c5
Shalem Raju <65066071+shalemking00@users.noreply.github.com>
2024-10-11 22:42:32 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/shalem" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <Home />
   );
 };
 
